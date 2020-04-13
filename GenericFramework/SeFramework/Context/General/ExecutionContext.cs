4fce85bf2036de195985e2d75a6bfa7c327d0088
TTCGlobalGeordieWinLove <geordie.winlove@ttcglobal.com>
2020-04-14 11:17:25 +1200

    Added Parent and ChildControl attributes. Added examples of these. Added screenshotting to the reports

--- GenericFramework/SeFramework/Context/General/ExecutionContext.cs
+++ GenericFramework/SeFramework/Context/General/ExecutionContext.cs
@@ -1,10 +1,12 @@
 ﻿using OpenQA.Selenium;
 using SeFramework.Config;
+using TechTalk.SpecFlow;
 
 namespace SeFramework.Context.General
 {
     public class ExecutionContext
     {
+        public ScenarioContext ScenarioContext { get; internal set; }
         public ConfigReader ConfigReader { get; internal set; }
         public IWebDriver Driver { get; set; }
     }
---
+++ GenericFramework/SeFramework/PageObject/NotificationBar.cs
@@ -0,0 +1,35 @@
+﻿using OpenQA.Selenium;
+using SeFramework.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeFramework.PageObject
+{
+	public class NotificationBar : BaseObject
+	{
+		private static NotificationBar Instance { get; set; }
+		private NotificationBar(IWebDriver driver, By iFrameId = null) : base(driver, iFrameId)
+		{
+		}
+
+		public static NotificationBar Get(IWebDriver driver)
+		{
+			if (Instance == null) Instance = new NotificationBar(driver);
+			return Instance;
+		}
+
+		protected override string Title => "";
+
+		[Parent(ByType.Id, "bar-notification")]
+		public enum Controls
+		{
+			[Control(ByType.ClassName, "content")]
+			Text,
+			[Control(ByType.ClassName, "close")]
+			Close
+		}
+	}
+}
